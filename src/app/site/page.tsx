4d9990269d325dc407d5ef343e4959febf0fc17b
Anjas-code <arifanjas.code@gmail.com>
2024-02-29 07:35:49 +0700

    feat: pricing card

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,16 +1,29 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { PRICING_CARD } from "@/lib/constans";
 import Image from "next/image";
-
+import Link from "next/link";
+import clsx from "clsx";
+import { Check } from "lucide-react";
 export default function Home() {
     return (
         <>
-            <section className="h-full w-full pt-36 relative items-center justify-center flex-col">
+            <section className="h-full w-full md:pt-44 mt-[-0px] relative flex items-center justify-center flex-col ">
                 <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
 
-                <p className="text-center">Run Your Agency, in one place </p>
+                <p className="text-center md:mt-40 md:text-4xl">
+                    Run your agency, in one place
+                </p>
 
-                <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative p-3">
-                    <h1 className="text-5xl text-center font-bold md:text-[120px]">
-                        Asset-Management
+                <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+                    <h1 className="text-7xl text-center font-bold md:text-[250px]">
+                        Managiere
                     </h1>
                 </div>
 
@@ -22,7 +35,80 @@ export default function Home() {
                         width={1200}
                         className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
                     />
-                    <div className="bottom-0 top-[50%] left-0 right-0 absolute bg-gradient-to-t dark:from-background z-10" />
+                    <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+                </div>
+            </section>
+
+            <section className="flex justify-center items-center flex-col gap-4 md:!mt-80 mt-[-60px]">
+                <h2 className="text-3xl text-center">
+                    Choose what fits you right
+                </h2>
+                <p className="text-muted-foreground text-center">
+                    Our straightforward pricing plans are tailored to meet Your
+                    needs. if {"you're"} not <br /> ready to commit, you can get
+                    started for free.
+                </p>
+                <div className="flex cards-center justify-center gap-4 flex-wrap mt-6">
+                    {PRICING_CARD.map((card) => (
+                        //WIP : wire up free product from stripe
+                        <Card
+                            key={card.priceId}
+                            className={clsx(
+                                "w-[300px] flex flex-col justify-between",
+                                {
+                                    "border-2 border-primary":
+                                        card.title === "Unlimited Saas",
+                                }
+                            )}
+                        >
+                            <CardHeader>
+                                <CardTitle
+                                    className={clsx("", {
+                                        "text-muted-foreground":
+                                            card.title !== "Unlimited Saas",
+                                    })}
+                                >
+                                    {card.title}
+                                </CardTitle>
+                                <CardDescription>
+                                    {card.description}
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="text-center">
+                                <span className="text-4xl font-bold">
+                                    {card.price}
+                                </span>
+                                <span className="text-muted-foreground">
+                                    /month
+                                </span>
+                            </CardContent>
+                            <CardFooter className="flex flex-col cards-start gap-4">
+                                <div>
+                                    {card.features.map((features) => (
+                                        <div
+                                            key={features}
+                                            className="flex gap-2 items-center"
+                                        >
+                                            <Check className="text-muted-foreground" />
+                                            <p>{features}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                                <Link
+                                    href={`/agency?plan=${card.priceId}`}
+                                    className={clsx(
+                                        "w-full text-center bg-primary p-2 rounded-md",
+                                        {
+                                            "!bg-muted-foreground":
+                                                card.title !== "Unlimited Saas",
+                                        }
+                                    )}
+                                >
+                                    Get Started
+                                </Link>
+                            </CardFooter>
+                        </Card>
+                    ))}
                 </div>
             </section>
         </>
--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -2,6 +2,8 @@ import { User } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/global/mode-toggle";
 
 type Props = {
     user?: null | User;
@@ -17,7 +19,7 @@ const Navigation = ({ user }: Props) => {
                     width={40}
                     height={40}
                 />
-                <span className="text-xl font-bold">Asset-management.</span>
+                <span className="text-xl font-bold">Managiere</span>
             </aside>
             <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
                 <ul className="flex items-center justify-center gap-8">
@@ -27,6 +29,17 @@ const Navigation = ({ user }: Props) => {
                     <Link href="#">Features</Link>
                 </ul>
             </nav>
+
+            <aside className="flex gap-2 items-center">
+                <Link
+                    href={"/agency"}
+                    className="bg-primary text-white py-2 px-4 rounded-md hover:bg-primary/80 transition-all"
+                >
+                    Login
+                </Link>
+                <UserButton />
+                <ModeToggle />
+            </aside>
         </div>
     );
 };
