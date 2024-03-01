14a895c62414d2d9f6a2e373b74eb22e105e3c14
Anjas-code <arifanjas.code@gmail.com>
2024-03-01 16:57:59 +0700

    chore: setup user login queries, check user agency etc

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const MainPage = () => {
-  return (
-    <div>Agency-dashboard</div>
-  )
-}
+const AgencyPage = async () => {
+    const agencyId = await verifyAndAcceptInvitation()
+    console.log(agencyId)
 
-export default MainPage
+    const user = await getAuthUserDetails()
+
+    return <div>Agency-dashboard</div>;
+};
+
+export default AgencyPage;
